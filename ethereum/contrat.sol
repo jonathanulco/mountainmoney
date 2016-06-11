b43ce1912023a5a14c9dc523ed2ef2197d558399
Austin <austing@gmail.com>
2016-06-11 16:06:30 +0200

    updated contract

--- ethereum/contrat.sol
+++ ethereum/contrat.sol
@@ -1,137 +1,125 @@
 contract Pot {
 
+    // Settings for contract, determined at initialization
+
+    // Multiplier: deposit multiplied by this to determine max loan
     uint public multiplier;
+
+    // Amount of time to wait after deposits before counting them
+    // in calculation of max loan
     uint public waitingWeeks;
-    uint public timeToPayBack;
+
+    // Hard limit to loans
     uint public maxLoan;
-    uint public rateOfBorrowing;
+
+    // For later
+    // uint public timeToPayBack;
+    // uint public rateOfBorrowing;
     // uint public rateOfUnanimityForNewMembers;
 
+    // Founder created contract
     address public founder;
-    address[] members;
-    address[] invited;
-    mapping(address => int) accountBalance; //signed
 
+    // Members are current members
+    enum MemberStatuses { Invited, Member }
+    mapping(address => MemberStatuses) memberStatus;
+    address[] memberList;
+
+    // Balances for each member - a signed integer, can be negative
+    mapping(address => int) accountBalance;
+
+    // Every deposit or withdrawal is a transaction; amountIntoPot
+    // is negative for withdrawals and positive for deposits.
     struct Transaction {
         uint    timestamp;
         int     amountIntoPot;
     }
 
+    // History of transactions for a user
     mapping(address => Transaction[]) accountHistory;
 
     // TRANSACTIONS: State-altering methods
 
-    function invite(address newMember){
-      if(msg.sender == founder){
-        invited.push(newMember);
-      }
+    modifier mustBeMember() {
+        if(memberStatus[msg.sender] != MemberStatuses.Member) throw;
     }
 
-    function join(){
-      var hasInvite = false;
-      for (uint i = 0; i < invited.length; i++) {
-        if(invited[i] == msg.sender){
-          hasInvite = true;
-          break;
-        }
-      }
-      if(!hasInvite){
-        return;
-      }
-
-      bool isAlreadyMember = false;
-      for (uint l = 0; l < members.length; l++) {
-        if(members[l] == msg.sender){
-          isAlreadyMember = true;
-          break;
-        }
-      }
-      if(isAlreadyMember){
-        return;
-      }
-      if(accountBalance[msg.sender] > 0){
-        // Already has account!
-        return;
-      }
-
-      // Everything's ok, let's add the member
-      accountBalance[msg.sender] = 0;
-      members.push(msg.sender);
-
+    modifier mustBeFounder() {
+        if(msg.sender != founder) throw;
     }
 
-    function deposit(){
-
-        bool isMember = false;
-        for (uint i = 0; i < members.length; i++) {
-          if(members[i] == msg.sender){
-            isMember = true;
-            break;
-          }
+    function invite(address newMember) mustBeFounder() {
+        var status = memberStatus[newMember];
+        if(status != MemberStatuses.Member && status != MemberStatuses.Invited){
+            memberStatus[newMember] == MemberStatuses.Invited;
         }
-        if(!isMember){
-          return;
-        }
+    }
 
+    function join() {
+        // Member who has been invited accepts invitation and
+        // Becomes a member
+        if(memberStatus[msg.sender] == MemberStatuses.Invited){
+            memberStatus[msg.sender] = MemberStatuses.Member;
+            memberList.push(msg.sender);
+        }
+    }
+
+    function deposit() mustBeMember() {
+        // Send money as value in the deposit function
+
+        // No point in recording the history of null transactions
         if(msg.value == 0){
           return;
         }
 
+        // Make transaction record for deposit
         accountHistory[msg.sender].push(Transaction(
           now,
           int(msg.value)
         ));
 
-        accountBalance[msg.sender] = accountBalance[msg.sender] + int(msg.value);
+        // Update account balance for member
+        accountBalance[msg.sender] += int(msg.value);
 
     }
 
-    function withdraw(uint amount){
-
-        bool isMember = false;
-        for (uint i = 0; i < members.length; i++) {
-          if(members[i] == msg.sender){
-            isMember = true;
-            break;
-          }
-        }
-        if(!isMember){
-          return;
-        }
+    function withdraw(uint amount) mustBeMember() {
+        // Withdraw deposits placed -- not more than what has
+        // been placed in the account. This can be done at any time.
 
         if(amount == 0) return;
 
+        // Check that the user has money on the account
+        // And that the amount they want to withdraw is less
+        // than their total balance.
         if(accountBalance[msg.sender] <= 0 || accountBalance[msg.sender] < int(amount)){
           return;
         }
 
+        // Check that there is globally enough money on the contract
+        // to make the transaction.
         if(this.balance < amount){
           return;
         }
 
+        // Send the money
         msg.sender.send(amount);
 
+        // Record the transaction
         accountHistory[msg.sender].push(Transaction(
           now,
           -int(amount)
         ));
 
+        // Update the member account balance
         accountBalance[msg.sender] = accountBalance[msg.sender] - int(amount);
 
     }
 
-    function loan(uint amount){
-
-        bool isMember = false;
-        for (uint i = 0; i < members.length; i++) {
-          if(members[i] == msg.sender){
-            isMember = true;
-            break;
-          }
-        }
-        if(!isMember){
-          return;
-        }
+    function loan(uint amount) mustBeMember() {
+        // Withdrawl an amount that is greater than the sum
+        // of deposits. Validation is more complex than for
+        // simple withdrawals.
 
         if(amount == 0) return;
 
@@ -139,50 +127,55 @@ contract Pot {
           return;
         }
 
-        var borrowBase = int(0);
+        int borrowBase;
+        // Consider all transactions in user's history
+        // @TODO Consider putting a hard limit on number of transactions
+        // in accountHistory, to not allow someone to consume all the gas
         for (uint j = 0; j < accountHistory[msg.sender].length; j++) {
-          if(accountHistory[msg.sender][j].amountIntoPot >= 0){
-            if (now < accountHistory[msg.sender][j].timestamp + waitingWeeks * 1 weeks){
-              continue;
+            // If the transaction we're considering is a deposit,
+            // check that it is older than six months; if it is not,
+            // do not take it into account. If it is, take it into account.
+
+            // If the transaction is a withdrawal or loan, it is taken
+            // into account no matter when it was.
+            bool A = accountHistory[msg.sender][j].amountIntoPot >= 0;
+            bool B = now < accountHistory[msg.sender][j].timestamp + waitingWeeks * 1 weeks;
+
+            if(!(A && B)){
+                borrowBase += accountHistory[msg.sender][j].amountIntoPot;
             }
-          }
-          borrowBase += accountHistory[msg.sender][j].amountIntoPot;
         }
 
+        // Once we have the base, we multiply it by the factor
+        // defined in the contract terms.
         var amountCanBorrow = borrowBase * int(multiplier);
         if(amountCanBorrow > int(this.balance)){
             return;
         }
 
+        // If the amount the user wishes to borrow is greater
+        // than the amount allowed, do not execute the operation
         if(int(amount) > amountCanBorrow){
             return;
         }
 
+        // If we got to here, everything's OK, send the money.
         msg.sender.send(amount);
 
+        // Record transaction
         accountHistory[msg.sender].push(Transaction(
           now,
           -int(amount)
         ));
 
+        // Update account balance
         accountBalance[msg.sender] = accountBalance[msg.sender] - int(amount);
 
     }
 
     // CALLS: Read only operations
 
-    function canWithdraw() returns (int){
-
-      bool isMember = false;
-      for (uint i = 0; i < members.length; i++) {
-        if(members[i] == msg.sender){
-          isMember = true;
-          break;
-        }
-      }
-      if(!isMember){
-        return 0;
-      }
+    function canWithdraw() mustBeMember() constant returns (int) {
 
       if(accountBalance[msg.sender] <= 0){
         return 0;
@@ -191,18 +184,7 @@ contract Pot {
 
     }
 
-    function canBorrow() returns (uint){
-
-      bool isMember = false;
-      for (uint i = 0; i < members.length; i++) {
-        if(members[i] == msg.sender){
-          isMember = true;
-          break;
-        }
-      }
-      if(!isMember){
-        return 0;
-      }
+    function canBorrow() mustBeMember() constant returns (uint) {
 
       var borrowBase = int(0);
       for (uint k = 0; k < accountHistory[msg.sender].length; k++) {
@@ -226,13 +208,6 @@ contract Pot {
       return amountCanBorrow;
 
     }
-
-    function numberOfMembers() returns (uint){
-
-      return members.length;
-
-    }
-
 
     // function myAccountHistory() constant returns (Transaction[]){
 
